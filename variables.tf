0026444babc7f20656e2dc3be2746b8fa191b69b
Bernard Adanlessossi <adanlessossi@hotmail.com>
2022-05-22 21:09:49 +0200

    fix: configure cluster repository and project

--- variables.tf
+++ variables.tf
@@ -34,7 +34,7 @@ variable "cluster_name" {
 variable "force_destroy" {
   description = "Flag to determine whether storage buckets get forcefully destroyed. If set to false, empty the bucket first in the aws s3 console, else terraform destroy will fail with BucketNotEmpty error"
   type        = bool
-  default     = true
+  default     = false
 }
 
 variable "is_jx2" {
